c57c2842f34f46e2f0287d77e27bd0f64aa4d511
pengshiyu.cc <pengshiyu.cc@bytedance.com>
2023-09-12 00:58:51 +0800

    trap dynamic programming

--- src/leetcode/trap.rs
+++ src/leetcode/trap.rs
@@ -5,6 +5,8 @@ use super::Solution;
 
 #[allow(dead_code)]
 impl Solution {
+    //单调栈，单调降
+    //可以考虑括号匹配，每次遇到有上升的柱子时，就知道可能有积水了，这时就应该关闭括号
     pub fn trap(height: Vec<i32>) -> i32 {
         let mut stack = Vec::new();
         let mut ans = 0;
@@ -24,6 +26,29 @@ impl Solution {
         }
         ans
     }
+
+    //朴素法，动态规划，求leftMax和rightMax
+    //leftMax = max(leftMax[i-1], height[i])
+    //rightMax = max(rightMax[i+1], height[i])
+    //trap[i] = min(leftMax[i], rightMax[i]) - height[i]
+    pub fn trap2(height: Vec<i32>) -> i32 {
+        let mut leftMax: Vec<i32> = vec![0; height.len()];
+        let mut rightMax: Vec<i32> = vec![0; height.len()];
+        leftMax[0] = height[0];
+        rightMax[height.len() - 1] = height[height.len() - 1];
+        for i in 1..height.len() {
+            leftMax[i] = std::cmp::max(leftMax[i - 1], height[i])
+        }
+        for i in (0..height.len() - 1).rev() {
+            rightMax[i] = std::cmp::max(rightMax[i + 1], height[i])
+        }
+
+        let mut res = 0;
+        for i in 0..height.len() {
+            res += std::cmp::min(leftMax[i], rightMax[i]) - height[i]
+        }
+        res
+    }
 }
 
 #[cfg(test)]
@@ -32,7 +57,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(6, Solution::trap(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
-        assert_eq!(9, Solution::trap(vec![4, 2, 0, 3, 2, 5]));
+        assert_eq!(6, Solution::trap2(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));
+        assert_eq!(9, Solution::trap2(vec![4, 2, 0, 3, 2, 5]));
     }
 }
